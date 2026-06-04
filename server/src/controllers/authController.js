d763ee1d1dcbaf594e059f4e47e0b5d2d771f5e2
jiteshborse <khushborse92@email.com>
2026-06-05 01:15:25 +0530

    initialize Express backend with JWT authentication and RBAC

---
+++ server/src/controllers/authController.js
@@ -0,0 +1,94 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const generateToken = require("../utils/generateToken");
+
+exports.register = async (req, res) => {
+
+    try {
+
+        const {
+            name,
+            email,
+            password,
+            role
+        } = req.body;
+
+        const existingUser =
+            await User.findOne({ email });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword =
+            await bcrypt.hash(password, 10);
+
+        const user =
+            await User.create({
+                name,
+                email,
+                password: hashedPassword,
+                role
+            });
+
+        res.status(201).json({
+            success: true,
+            user
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+exports.login = async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+        const user =
+            await User.findOne({ email });
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const isMatch =
+            await bcrypt.compare(
+                password,
+                user.password
+            );
+
+        if (!isMatch) {
+            return res.status(401).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const token =
+            generateToken(
+                user._id,
+                user.role
+            );
+
+        res.json({
+            success: true,
+            token,
+            user
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
---
+++ server/src/models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+
+        password: {
+            type: String,
+            required: true
+        },
+
+        role: {
+            type: String,
+            enum: ["admin", "recruiter", "candidate"],
+            default: "candidate"
+        },
+
+        status: {
+            type: String,
+            default: "active"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = mongoose.model("User", userSchema);
